00e3b9596e1c1a79a9b4a722ff5810d16199a94e
Emrick19 <emricky17@gmail.com>
2021-06-22 23:35:29 +0100

    Update User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,21 +1,28 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+import { Field, ObjectType, ID } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity()
-export class User extends BaseEntity{
+export class User extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Field()
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string;
+  @Field()
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string;
+  @Field()
+  @Column("text", { unique: true })
+  email: string;
 
-    @Column("text", { unique: true})
-    email: string;
-     
-    @Column()
-    password: string;
-
+  @Field()
+  name: string;
+  
+  @Column()
+  password: string;
 }
