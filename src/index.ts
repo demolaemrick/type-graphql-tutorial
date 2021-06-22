5c210e34e3932a6b8266272e15f590af9d4997a8
Emrick19 <emricky17@gmail.com>
2021-06-22 02:28:22 +0100

    Create first typegraphql server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express'
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+const PORT = process.env.PORT || 8080;
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello world!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const server = new ApolloServer({
+    schema  
+  });
+
+  const { url } = await server.listen(PORT);
+  console.log(`🚀 Server is running, GraphQL Playground available at ${url}graphql`)
+};
+
+main();
