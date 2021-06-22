fec3e8665c6fb76e7b71ce0ceda22ddf0546f9af
Emrick19 <emricky17@gmail.com>
2021-06-22 23:35:51 +0100

    Create register resolver

---
+++ src/modules/user/register.ts
@@ -0,0 +1,36 @@
+import { Resolver, Query, Mutation, Arg, FieldResolver, Root } from "type-graphql";
+import * as bcrypt from "bcrypt";
+
+import { User } from "../../entity/User";
+
+@Resolver(User)
+export class RegisterResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello world!";
+  }
+
+  @FieldResolver()
+  async name(@Root() parent: User) {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    return user;
+  }
+}
