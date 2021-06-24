2343afb3366bba759548eab836b63f5d6c41614d
Emrick19 <emricky17@gmail.com>
2021-06-24 09:18:07 +0100

    Create LoginResolver

---
+++ src/modules/user/Login.ts
@@ -0,0 +1,24 @@
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import bcrypt from "bcrypt";
+import { UserInputError } from "apollo-server-express"
+
+import { User } from "../../entity/User";
+import { MyContext } from "../types/MyContext";
+
+@Resolver(User)
+export class LoginResolver {
+  @Mutation(() => User, { nullable: true})
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
+    const user = await User.findOne({email: email});
+    if(!user) throw new UserInputError("User does not exist")
+    const match = await bcrypt.compare(password, user.password);
+    if(!match) throw new UserInputError("Incorrect password")
+
+    ctx.req.session.userId = user.id
+    return user
+  }
+}
