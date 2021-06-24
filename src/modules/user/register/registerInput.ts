98a7ddef80df71500131e85d33e845a2bd44a3ad
Emrick19 <emricky17@gmail.com>
2021-06-24 05:26:13 +0100

    Add validation

--- src/modules/user/register/registerInput.ts
+++ src/modules/user/register/registerInput.ts
@@ -1,16 +1,22 @@
 import { InputType, Field } from "type-graphql";
+import { Length, IsEmail } from "class-validator";
+import { IsEmailAlreadyExist } from "./isEmailAlreadyExist";
 
 @InputType()
 export class RegisterInput {
-    @Field() 
-    firstName: string;
+  @Field()
+  @Length(1, 255)
+  firstName: string;
 
-    @Field() 
-    lastName: string;
+  @Field()
+  @Length(1, 255)
+  lastName: string;
 
-    @Field() 
-    email: string
+  @Field()
+  @IsEmail()
+  @IsEmailAlreadyExist({message: "Email already exists"})
+  email: string;
 
-    @Field() 
-    password: string
+  @Field()
+  password: string;
 }
