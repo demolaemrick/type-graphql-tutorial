6506b3b72ea4f1a420c4075b71a0c678b33fef21
Emrick19 <emricky17@gmail.com>
2021-06-24 09:17:01 +0100

    Add Login and Me Resolvers

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,54 @@ import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors"
 
-import { RegisterResolver } from "./modules/user/register";
+import { redis } from "./redis";
+
+import { RegisterResolver } from "./modules/user/Register";
+import { LoginResolver } from "./modules/user/Login"
+import { MeResolver } from "./modules/user/Me"
 
 const PORT = process.env.PORT || 8080;
 
 const main = async () => {
   await createConnection();
 
-  const app = express();
-
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
   });
 
   const server = new ApolloServer({
     schema,
+    context: ({req}: any) => ({req})
   });
+
+  const app = express();
+
+  app.use(cors({
+    credentials: true,
+    origin: 'https://localhost:3000'
+  }))
+  const RedisStore = connectRedis(session); 
+
+  const sessionOption: session.SessionOptions = {
+    store: new RedisStore({
+      client: redis as any,
+    }),
+    name: "qid",
+    secret: "mysessionsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+    },
+  };
+
+  app.use(session(sessionOption));
 
   server.applyMiddleware({ app });
 
