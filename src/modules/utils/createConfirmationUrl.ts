8a972c8fd549e810a1a7c2299c2fe97bc65c7ffa
Emrick19 <emricky17@gmail.com>
2021-06-26 03:29:17 +0100

    Update createConfirmation utility fn

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,9 +1,17 @@
-import { v4 as uuid } from 'uuid'
-import { redis } from '../../redis'
+import { v4 as uuid } from "uuid";
+import { redis } from "../../redis";
 
-export const createConfirmationUrl = async (userId: number) => {
-    const token = uuid()
-    await redis.set(token, userId, "ex", 60 * 60 * 24); //1 day expiration
+export const createConfirmationUrl = async (
+  userId: number,
+  redisPrefix: string
+) => {
+  const token = uuid();
+  await redis.set(redisPrefix + token, userId, "ex", 60 * 60 * 24); //1 day expiration
 
-    return `https://localhost:3000/user/confirm/${token}`
-}
+  let url = `https://localhost:3000/user/confirm/${token}`;
+ 
+  if (redisPrefix === "forgot-password") {
+    url = `https://localhost:3000/user/change-password/${token}`;
+  }
+  return url;
+};
